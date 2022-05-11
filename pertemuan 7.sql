9bcb7771109f8abb10731701e1f6a0d4d2e1c6ba
fahridika <96968769+fahridika@users.noreply.github.com>
2022-05-11 14:34:43 +0700

    Update pertemuan 7.sql

--- pertemuan 7.sql
+++ pertemuan 7.sql
@@ -9,3 +9,14 @@ CREATE TABLE mhspolines (
     progdi CHAR(15),
     sum_ukt INT(9) DEFAULT '7000000'
     );
+#data MAHASISWA
+INSERT INTO mhspolines (nim , nama , jurusan, progdi, sum_ukt)
+    VALUES
+        ('33420401', 'Ahmad', 'elektro', '1', '1', '1'),
+        ('33420402', 'Logika', 'elektro', '1', '2', '1'),
+        ('33420403', 'Basic', 'elektro', '1', '2', '1'),
+        ('33420404', ' Data', 'akuntasi', '2', '2', '1'),
+        ('33420405', 'Pascal', 'akuntasi', '2', '2', '1'),
+        ('33420406', 'Dasar', 'akuntasi', '2', '1', '1'),
+        ('33420407', 'Karier', 'sipil', '5', '3', '1'),
+        ('33420408', 'Akhirun', 'sipil', '6', '3', '1');
