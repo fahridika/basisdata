edd39fe6ab5cf7e5c1faca05d2105e4e8c0651af
Sindu Masri Priandana <73923369+SinduMP@users.noreply.github.com>
2022-05-11 14:27:04 +0700

    Update pertemuan 7.sql

--- pertemuan 7.sql
+++ pertemuan 7.sql
@@ -2,10 +2,10 @@ use mahasiswa;
 #1.	sebelum melakukan ke lima printah tersebut, anda sudah memiliki atau membuat sebuah 
 #table yang berisi data mahasiswa berupa nim, nama, jurusan, progdi dan sumbangan uang kuliah.
 
-CREATE TABLE mkuliah (
+CREATE TABLE mhspolines (
     nim CHAR(7) PRIMARY KEY UNIQUE NOT NULL,
     nama CHAR(20) NOT NULL,
-    jurusan (2) NOT NULL,
-    progdi CHAR(1),
-    sum_ukt CHAR(9) DEFAULT '1'
+    jurusan CHAR(15) NOT NULL,
+    progdi CHAR(15),
+    sum_ukt INT(9) DEFAULT '7000000'
     );
