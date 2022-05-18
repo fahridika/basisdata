690c5c54fbb96e7d23101e65adb7260545b6450b
fahridika <96968769+fahridika@users.noreply.github.com>
2022-05-18 10:35:02 +0700

    Update uts.sql

--- uts.sql
+++ uts.sql
@@ -1,1 +1,8 @@
+#1. Tuliskan perintah untuk membuat database dengan nama TOKOXXXXX (dimana XXXXX adalah 5 digit nim terakhir Anda) !
+Perintah
+mysql -u root
+# membuat database
+create database toko20415;
+
+
 
