8ca68bbc0eb193de2427f1faf39e43590d34f9df
MUHAMMAD FAHRI DIKA YUDA <96968769+fahridika@users.noreply.github.com>
2022-05-25 13:57:20 +0700

    Update pertemuan 8.sql

--- pertemuan 8.sql
+++ pertemuan 8.sql
@@ -1,1 +1,11 @@
 
+INSERT INTO film (kode , judul , tahun)
+    VALUES
+        ('3342041', 'Ahmad', 'elektro'),
+        ('3342042', 'Logika', 'elektro'),
+        ('3342043', 'Basic', 'elektro'),
+        ('3342044', 'Data', 'akuntasi'),
+        ('3342045', 'Pascal', 'akuntasi'),
+        ('3342046', 'Dasar', 'akuntasi'),
+        ('3342047', 'Karier', 'sipil'),
+        ('3342048', 'Akhirun', 'sipil');
