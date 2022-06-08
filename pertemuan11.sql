c751a75773dbef3ef8d7878aa700479bf849e6a7
MUHAMMAD FAHRI DIKA YUDA <96968769+fahridika@users.noreply.github.com>
2022-06-08 14:48:57 +0700

    Update pertemuan11.sql

--- pertemuan11.sql
+++ pertemuan11.sql
@@ -1,1 +1,5 @@
-
+SELECT judul_buku, isbn, nama_pengarang, nama_penerbit, harga_buku, stok
+FROM tbuku_fahri s
+INNER JOIN tpenerbit a on s.id_tpenerbit = a.id_penerbit
+INNER JOIN tpengarang b on s.id_tpengarang = a.id_pengarang
+;
