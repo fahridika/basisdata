d8895818e1e2d06c8cac69daf184b5ab4a1f620f
MUHAMMAD FAHRI DIKA YUDA <96968769+fahridika@users.noreply.github.com>
2022-06-08 14:51:50 +0700

    Update pertemuan11.sql

--- pertemuan11.sql
+++ pertemuan11.sql
@@ -1,5 +1,9 @@
-SELECT judul_buku, isbn, nama_pengarang, nama_penerbit, harga_buku, stok
+Create Table film (
+kode_film Char(5) Primary Key Not Null,
+judul Char(30) Not Null,
+tahun Char(4) Not Null);
+SELECT judul_buku, isbn, nama_pengarang, nama_penerbit, harga_buku, stock
 FROM tbuku_fahri s
 INNER JOIN tpenerbit a on s.id_tpenerbit = a.id_penerbit
-INNER JOIN tpengarang b on s.id_tpengarang = a.id_pengarang
+INNER JOIN tpengarang b on s.id_tpengarang = b.id_pengarang
 ;
