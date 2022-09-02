784bc3b1a1753d5e6aaefe3c993692df373882fb
MUHAMMAD FAHRI DIKA YUDA <96968769+fahridika@users.noreply.github.com>
2022-09-02 11:31:35 +0700

    Create s1Ar.sql

---
+++ s1Ar.sql
@@ -0,0 +1,38 @@
+Create database s1ar;
+
+use s1ar;
+
+CREATE TABLE user(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+npk CHAR (6) UNIQUE ,
+email CHAR (20) UNIQUE,
+PASSWORD CHAR(12) NOT NULL);
+
+CREATE TABLE user_detail(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+nama CHAR(32),
+email CHAR(30) NOT NULL UNIQUE,
+npk CHAR(6) UNIQUE,
+no_hp INT(14),
+tmp_lahir CHAR(20),
+tgl_lahir DATE,
+departemen CHAR(4),
+gol CHAR(3),
+sta_kerja CHAR(4),
+sta_nikah CHAR(2),
+sta_pajak CHAR(4),
+no_bpjs_tk CHAR(20),
+no_bpjs_kes CHAR(20),
+no_dpa CHAR(20),
+no_npwp CHAR (20),
+no_ktp CHAR (16),
+alamat_dom TEXT,
+alamat_ktp TEXT,
+nama_istri CHAR (25),
+jml_anak INT,
+nama_anak1 CHAR (25),
+nama_anak2 CHAR (25),
+nama_anak3 CHAR (25));
+
+
